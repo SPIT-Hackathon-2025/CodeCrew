6ebec4673a74439d636998aa5e2d1dbc8825ba1f
Prasad Kute <prasadkute0708@gmail.com>
2025-02-08 22:35:03 +0530

    trying truffle

--- backend/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error('Error:', err.message);
-    res.status(500).json({ success: false, message: 'Server Error' });
-  };
-  
-  export default errorHandler;
-  
--- backend/routes/webhookRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { handleWebhook } from '../controllers/webhookController.js';
-
-const router = express.Router();
-
-router.post('/webhooks', bodyParser.raw({ type: 'application/json' }), handleWebhook);
-
-export default router;
